e9ac2058ed152d03fd0e01eea052993c17ad21a5
Tahir Mehmood <tahir.mehmood@hyperisland.se>
2025-05-06 13:43:33 +0200

    feat:banner components added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styles from './banner.module.css';
+import bannerImage from '../../assets/images/banner-image-temporary.png';
 
 const Banner = () => {
   return (
-    <div>
-      <p>i am banner</p>
-    </div>
-  )
-}
+    <section
+      className={`${styles.banner} container`}
+      style={{ backgroundImage: `url(${bannerImage})` }}
+    >
+      <div className={styles.textBox}>
+        <h1 className={styles.text}>From Memory to Meaning</h1>
+      </div>
+    </section>
+  );
+};
 
-export default Banner
+export default Banner;
